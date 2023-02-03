896205b086832d9429bb2e269faafa0106df36d9
Giorgi <Gio.Chiaberashvili@gmail.com>
2023-02-03 15:14:13 +0400

    Completed

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
+const Employee = require("./employee")
+
 // Your code here
+
+const JohnWick = new Employee("John Wick", "Dog Lover")
+
+setTimeout(JohnWick.sayName.bind(JohnWick), 2000);
+
+setTimeout(JohnWick.sayOccupation.bind(JohnWick), 3000);
